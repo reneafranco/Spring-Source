891b5eb2daeae9507878d5e112a57bce2ff92c3e
renea <reneadonayfrancoperez@gmail.com>
2023-12-17 13:34:38 -0500

    updates

--- 4_dependency_injection/3_dependency_injection_annotations/colours/src/main/java/com/deviro/maven/services/impl/SpanishBluePrinter.java
+++ 4_dependency_injection/3_dependency_injection_annotations/colours/src/main/java/com/deviro/maven/services/impl/SpanishBluePrinter.java
@@ -1,9 +1,9 @@
 package com.deviro.maven.services.impl;
 
 import com.deviro.maven.services.BluePrinter;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class SpanishBluePrinter implements BluePrinter {
 
   @Override
